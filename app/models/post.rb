865baef8fd3f2feb5ad357c0173d52a17c8626c7
Austin Marsh <redausty@live.com>
2023-07-28 16:34:07 -0400

    adding like button

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
   validates :images, presence: true, blob: { content_type: :web_image }
+
+  has_many :likes
+  has_many :likers, through: :likes, source: :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   root 'home#index'
+  post "toggle_like", to: "likes#toggle_like", as: :toggle_like
 
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
