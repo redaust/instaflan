342d844aa4614a0638e91ed23b93ad98ebe62723
Austin Marsh <redausty@live.com>
2023-07-28 18:03:24 -0400

    adding turbo refresh to like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,11 +7,20 @@ def toggle_like
 	else
 		@post.likes.create(user: current_user)
 	end
+
+	respond_to do |format|
+	format.turbo_stream do
+		render turbo_stream: turbo_stream.replace(
+			"post#{@post.id}actions",
+			partial: "posts/post_actions",
+			locals: {post: @post}
+			)	
+		end
+	end
 end
 
 private
 def set_post
 	@post = Post.find(params[:post_id])
 end
-
 end
