a32083007b1c614de4eaf21b2ed98618740df39a
Austin Marsh <redausty@live.com>
2023-08-01 22:44:02 -0400

    adding turbo to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,16 @@ class CommentsController < ApplicationController
 	before_action :set_post, only: [:create]
 	def create
 		@comment = @post.comments.create(user: current_user, body: params[:comment_body])
+
+		respond_to do |format|
+			format.turbo_stream do 
+				render turbo_stream: turbo_stream.replace(
+					"post#{@post.id}comments",
+					partial: "posts/post_comments",
+					locals: {post: @post}
+					)
+			end	
+		end
 	end
 
 	def destroy
@@ -9,11 +19,11 @@ class CommentsController < ApplicationController
 		if (@comment.user == current_user)
 			@comment.destroy
 		end
-
 	end
 
 
 	private
 	def set_post
 		@post = Post.find(params[:post_id])
+end
 end
