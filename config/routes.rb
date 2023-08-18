631ed8cc6ae90e0ce5d161cb622cac7b27658cbd
Austin Marsh <redausty@live.com>
2023-08-18 14:33:08 -0400

    adding profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post "toggle_like", to: "likes#toggle_like", as: :toggle_like
 
   resources :comments, only: [:create, :destroy]
-
+  resources :users, only: [:show]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
